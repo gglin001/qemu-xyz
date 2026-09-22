23bceb5bf24e895aada888f68f6063bb84ed1ceb
Allen Guo <guosonglin001@gmail.com>
2026-09-22 14:42:55 +0800

    setup

--- alpine_osx_arm64.sh
+++ alpine_osx_arm64.sh
@@ -20,6 +20,73 @@ qemu-system-aarch64 \
   -device virtio-net-pci,netdev=net0 \
   -nographic
 
-setup-alpine
+# root
+# nopasswd
+
+# setup-alpine
+
+cat <<'EOF' >auto-install.sh
+#!/bin/sh
+set -e
+
+cat > /tmp/answerfile << 'ANS'
+KEYBOARDOPTS="us us"
+HOSTNAMEOPTS="-n alpine"
+INTERFACESOPTS="auto lo
+iface lo inet loopback
+
+auto eth0
+iface eth0 inet dhcp
+"
+DNSOPTS=""
+TIMEZONEOPTS="-z Asia/Shanghai"
+PROXYOPTS="none"
+APKREPOSOPTS="https://mirrors.tuna.tsinghua.edu.cn/alpine/v3.24/main"
+SSHDOPTS="-c openssh"
+NTPOPTS="-c chrony"
+USEROPTS="none"
+DISKOPTS="-m sys /dev/vda"
+ANS
+
+export ERASE_DISKS="/dev/vda"
+setup-alpine -f /tmp/answerfile
+EOF
+echo "root:0" | chpasswd
+sh auto-install.sh
+apk update
+
+echo "https://mirrors.tuna.tsinghua.edu.cn/alpine/v3.24/community" >>/etc/apk/repositories
+apk update
+
+adduser -D -s /bin/ash agi
+echo "agi:0" | chpasswd
+
+addgroup agi wheel
+apk add doas
+mkdir -p /etc/doas.d
+echo "permit persist :wheel" >/etc/doas.d/doas.conf
+
+mkdir -p /home/agi/.ssh
+cat <<'EOF' >/home/agi/.ssh/authorized_keys
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM/UyG2mYh9otEF8mf6WX4eAQq4zoRup+/XbAq7W750/ allen@allens-Mac-mini.local
+EOF
+chmod 700 /home/agi/.ssh
+chmod 600 /home/agi/.ssh/authorized_keys
+chown -R agi:agi /home/agi/.ssh
+mkdir -p /root/.ssh
+cat <<'EOF' >/root/.ssh/authorized_keys
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM/UyG2mYh9otEF8mf6WX4eAQq4zoRup+/XbAq7W750/ allen@allens-Mac-mini.local
+EOF
+chmod 700 /root/.ssh
+chmod 600 /root/.ssh/authorized_keys
+chown -R root:root /root/.ssh
+
+# root
+# 0
+
+# ssh -p 2222 agi@127.0.0.1 -i ~/.ssh/id_ed25519
+ssh -p 2222 agi@127.0.0.1
+# agi
+# 0
 
 ###############################################################################
